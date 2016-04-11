136c5a4a06f47e4c6350842728ac67e72475b687
Пользователь <>
2016-04-11 10:00:38 +0300

    первый файл

---
+++ table.php
@@ -0,0 +1,249 @@
+<!--BEGIN manufacturer DropDown List -->
+<table style="width: 100%;" border="0" align="center">
+    <tbody>
+    <tr align="center" valign="middle">
+        <td>
+            <a href="/derbystar.html"><img src="images/images/derby1.jpg" alt="derbystar" width="85" height="30"
+                                           style="border: 2px solid #000000;"
+                                           onmouseover="this.src='images/images/derby.png';"
+                                           onmouseout="this.src='images/images/derby1.jpg';"/></a>
+        </td>
+        <td>
+            <a href="/joma.html"><img src="images/images/joma1.jpg" alt="joma" width="85" height="30"
+                                      onmouseout="this.src='images/images/joma1.jpg';"
+                                      onmouseover="this.src='images/images/joma.png';"
+                                      style="border: 2px solid #000000;"/></a>
+        </td>
+        <td>
+            <a href="/magazin/select.html"><img src="images/images/select1.jpg" alt="select" width="85" height="30"
+                                                onmouseout="this.src='images/images/select1.jpg';"
+                                                onmouseover="this.src='images/images/select.png';"
+                                                style="border: 2px solid #000000;"/></a>
+        </td>
+        <td>
+            <a href="magazin/winner.html"><img src="images/images/winner1.jpg" alt="winner" width="85" height="30"
+                                               onmouseout="this.src='images/images/winner1.jpg';"
+                                               onmouseover="this.src='images/images/winner.png';"
+                                               style="border: 2px solid #000000;"/></a>
+        </td>
+        <td>
+            <a href="?keyword=Adidas&amp;Search=%D0%9F%D0%BE%D0%B8%D1%81%D0%BA&amp;Itemid=1&amp;option=com_virtuemart&amp;page=shop.browse"><img
+                    src="images/images/adidas1.jpg" alt="adidas" width="85" height="30"
+                    onmouseout="this.src='images/images/adidas1.jpg';"
+                    onmouseover="this.src='images/images/adidas.gif';" style="border: 2px solid #000000;"/></a>
+        </td>
+        <td>
+            <a href="?keyword=penalty&amp;Search=Поиск&amp;Itemid=1&amp;option=com_virtuemart&amp;page=shop.browse"><img
+                    src="images/images/penalty1.jpg" alt="penalty" width="85" height="30"
+                    onmouseout="this.src='images/images/penalty1.jpg';"
+                    onmouseover="this.src='images/images/penalty.jpg';" style="border: 2px solid #000000;"/></a>
+        </td>
+    </tr>
+    </tbody>
+</table>
+<div class="productthumb">
+    <link rel="stylesheet" href="/modules/mod_tpindexvm/tmpl/tpindexvm.css" type="text/css"/>
+    <table style="width: 100%;" border="0" cellpadding="0" cellspacing="0" class="table table-condensed">
+        <tbody>
+        <tr>
+            <td align="center" valign="middle" width="33%">
+                <div class="pthumb"><a href="futbolnie-myachi.html" title="Футбольные мячи" id="productthumbbig4"
+                                       class="productthumbbigtitle"><span>Футбольные мячи</span></a>
+                    <a href="/futbolnie-myachi.html" title="Футбольные мячи" id="productthumbbig4"
+                       class="productthumbbig">
+                        <div class="pthumbimg"><br/>
+                            <p><img src="images/_________________52afed8044c41_150x150.png"
+                                    alt="Мяч футбольный Winner Super Nova FIFA" height="170" width="170"/></p>
+                        </div>
+                    </a>
+                </div>
+            </td>
+            <td align="center" valign="middle" width="33%">
+                <div class="pthumb"><a href="/futzalnie-myachi.html" title="Футзальные мячи" id="productthumbbig5"
+                                       class="productthumbbigtitle"><span>Футзальные мячи</span></a>
+                    <a href="/futzalnie-myachi.html" title="Футзальные мячи" id="productthumbbig5"
+                       class="productthumbbig">
+                        <div class="pthumbimg"><br/>
+                            <p><img src="images/show_image_in_imgtag.png" alt="Мяч футзальный Winner Match Sala"
+                                    height="170" width="170"/></p>
+                        </div>
+                    </a>
+                </div>
+            </td>
+            <td align="center" valign="middle" width="33%">
+                <div class="pthumb"><a href="/magazin/spetsialnie-myachi.html" title="Специальные мячи"
+                                       id="productthumbbig4" class="productthumbbigtitle"><span>Специальные мячи </span></a>
+                    <a href="/magazin/spetsialnie-myachi.html" title="Специальные мячи" id="productthumbbig4"
+                       class="productthumbbig"> </a>
+                    <a href="/magazin/spetsialnie-myachi.html" title="Специальные мячи" class="productthumbbig">
+                        <div class="pthumbimg"><br/>
+                            <p><img src="images/show_image_in_imgtag1111.png"
+                                    alt="яч тренировочный футбольный Winner Technics" height="170" width="150"/></p>
+                        </div>
+                    </a>
+                </div>
+            </td>
+        </tr>
+        <tr>
+            <td align="center" valign="middle" width="33%">
+                <div class="pthumb"><a href="/voleybolnie-myachi.html" title="Волейбольные мячи" id="productthumbbig2"
+                                       class="productthumbbigtitle"><span>Волейбольные мячи</span></a>
+                    <a href="/voleybolnie-myachi.html" title="Волейбольные мячи" id="productthumbbig2"
+                       class="productthumbbig">
+                        <div class="pthumbimg"><br/>
+                            <p><img src="images/vc5.jpg" height="170" width="170" alt="Волейбольные мячи"/></p>
+                        </div>
+                    </a>
+                </div>
+            </td>
+            <td align="center" valign="middle" width="33%">
+                <div class="pthumb"><a href="/basketbolnie-myachi.html" title="Баскетбольные мячи" id="productthumbbig1"
+                                       class="productthumbbigtitle"><span>Баскетбольные мячи</span></a>
+                    <a href="/basketbolnie-myachi.html" title="Баскетбольные мячи" id="productthumbbig1"
+                       class="productthumbbig">
+                        <div class="pthumbimg"><br/>
+                            <p><img src="images/Grippy-двухцв.jpg" height="170" width="170" alt="Баскетбольные мячи"/>
+                            </p>
+                        </div>
+                    </a>
+                </div>
+            </td>
+            <td align="center" valign="middle" width="33%">
+                <div class="pthumb"><a href="/myachi-dlya-gandbola.html" title="Мячи для гандбола" id="productthumbbig3"
+                                       class="productthumbbigtitle"><span>Мячи для гандбола</span></a>
+                    <a href="/myachi-dlya-gandbola.html" title="Мячи для гандбола" id="productthumbbig3"
+                       class="productthumbbig">
+                        <div class="pthumbimg"><br/>
+                            <p>
+                                <img
+                                    src="/components/com_virtuemart/shop_image/category/_________________4f0db9d1cc7fc.png"
+                                    alt="Мячи для гандбола" width="170" height="170"/></p>
+                        </div>
+                    </a>
+                </div>
+            </td>
+        </tr>
+        <tr>
+            <td align="center" valign="middle" width="33%">
+                <div class="pthumb"><a href="magazin/futbolnaya-obuv.html" title="Футзальная обувь"
+                                       id="productthumbbig4" class="productthumbbigtitle"><span>Футзальная обувь</span></a>
+                    <a href="/magazin/futbolnaya-obuv.html" title="Футзальная обувь" id="productthumbbig4"
+                       class="productthumbbig">
+                        <div class="pthumbimg"><br/>
+                            <p><img src="/images/obuv.png" alt="Футзальная обувь" height="170" width="170"/></p>
+                        </div>
+                    </a>
+                </div>
+            </td>
+            <td align="center" valign="middle" width="33%">
+                <div class="pthumb"><a href="magazin/sorokonozhki.html" title="Сороконожки" id="productthumbbig5"
+                                       class="productthumbbigtitle"><span>Сороконожки</span></a>
+                    <a href="magazin/sorokonozhki.html" title="Сороконожки" id="productthumbbig5"
+                       class="productthumbbig">
+                        <div class="pthumbimg"><br/>
+                            <p><img src="/images/images/sorokon.jpg" alt="Сороконожки" width="170" height="170"
+                                    border="0"/></p>
+                        </div>
+                    </a>
+                </div>
+            </td>
+            <td align="center" valign="middle" width="33%">
+                <div class="pthumb"><a href="magazin/butsi-futbolnie.html" title="Футбольные бутсы"
+                                       id="productthumbbig4" class="productthumbbigtitle"><span>Футбольные бутсы</span></a>
+                    <a href="magazin/butsi-futbolnie.html" title="Футбольные бутсы" id="productthumbbig4"
+                       class="productthumbbig">
+                        <div class="pthumbimg"><br/>
+                            <p><img src="/images/images/byts.jpg" alt="Футбольные бутсы" height="170" width="170"/></p>
+                        </div>
+                    </a>
+                </div>
+            </td>
+        </tr>
+        <tr>
+            <td align="center" valign="middle" width="33%">
+                <div class="pthumb"><a href="magazin/futbolnaya-forma.html" title="Футбольная форма"
+                                       id="productthumbbig4" class="productthumbbigtitle"><span>Футбольная форма</span></a>
+                    <a href="magazin/futbolnaya-forma.html" title="Футбольная форма" id="productthumbbig4"
+                       class="productthumbbig">
+                        <div class="pthumbimg"><br/>
+                            <p><img src="/images/images/forma.jpg" alt="Футбольная форма" height="170" width="170"/></p>
+                        </div>
+                    </a>
+                </div>
+            </td>
+            <td align="center" valign="middle" width="25">
+                <div class="pthumb"><a href="/magazin/perchatki-vratarskie.html" title="Вратарские перчатки"
+                                       id="productthumbbig1"
+                                       class="productthumbbigtitle"><span>Вратарские перчатки</span></a>
+                    <a href="/magazin/perchatki-vratarskie.html" title="Вратарские перчатки" id="productthumbbig1"
+                       class="productthumbbig">
+                        <div class="pthumbimg"><br/>
+                            <p><img src="images/show_image_in_imgtag.jpeg" alt="show image in imgtag" width="170"
+                                    height="170"/></p>
+                        </div>
+                    </a>
+                </div>
+            </td>
+            <td align="center" valign="middle" width="33%">
+                <div class="pthumb"><a href="/aksessuari.html" title="Аксессуары" id="productthumbbig0"
+                                       class="productthumbbigtitle"><span>Аксессуары</span></a>
+                    <a href="/aksessuari.html" title="Аксессуары" id="productthumbbig0" class="productthumbbig">
+                        <div class="pthumbimg"><br/>
+                            <p><img src="images/_________________50b7b19b2d3cd.jpg" height="175" width="137"
+                                    alt="Аксессуары"/></p>
+                        </div>
+                    </a>
+                </div>
+            </td>
+        </tr>
+        </tbody>
+    </table>
+</div>
+<div>&nbsp;</div>
+<h1>Купить футбольный мяч в Днепропетровске</h1>
+<div>
+    <p style="text-align: justify; text-indent: 18pt;">В нашем интернет магазине Вы можете <strong>купить футбольный
+            мяч</strong>, а также <strong>мяч </strong>для командных игр. Мы осуществляем продажу <strong>в
+            Днепропетровске</strong> и по Украине.</p>
+    <p style="text-align: justify; text-indent: 18pt;">Самым популярным после проведения ЕВРО-2012 являются футбольные
+        мячи. Сегодня <strong>футбольные мячи &nbsp;</strong>можно <strong>купить в Днепропетровске</strong>, не выходя
+        из дома, вам достаточно лишь заполнить в виртуальном режиме соответствующую заявку. Купленный Вами мяч будет
+        оперативно доставлен нашей курьерской службой в любую точку Украины.</p>
+    <p style="text-align: justify; text-indent: 18pt;">Выбирая футбольный мяч необходимо учитывать, где ВЫ будите
+        играть. Мячи для футбола выпускаются 5 размеров и делятся на <b>футбольные</b> и <b>футзальные.</b></p>
+    <p style="text-align: justify; text-indent: 18pt;"><a href="/futbolnie-myachi.html"><b>Футбольные мячи </b></a>–
+        предназначены для игры в футбол на стадионе.<b> </b>Они<b> </b>имеют традиционный отскок, что затрудняет их
+        использование в зале или площадках для мини-футбола. Самый популярный размер <strong>футбольного мяча</strong> –
+        5 размер (68-70 см.).</p>
+    <p style="text-align: justify; text-indent: 18pt;"><a href="/futzalnie-myachi.html"><b>Футзальные мячи </b></a>–
+        предназначены для игры в мини-футбол. Они имеют приглушенный (низкий) отскок, поэтому их значительно легче
+        контролировать, чем обычные футбольные мячи со стандартным отскоком. Самый популярный размер футзального мяча –
+        4 размер (62-64 см.).</p>
+    <p style="text-indent: 18pt;">Кроме того различают три вида мячей для футбола: тренировочный, матчевый и
+        профессиональный.</p>
+    <p style="text-indent: 18pt;">Тренировочные мячи имеют низкую стоимость и прекрасно подходят для дворовой
+        команды.</p>
+    <p style="text-indent: 18pt;">Матчевые мячи – в отличие от тренировочных мячей имеют более износостойкое покрытие.
+        Некоторые модели проходят сертификацию IMS. Матчевые мячи подходят для проведения тренировок и соревнований, как
+        любителей, так и клубов.</p>
+    <p style="text-indent: 18pt;">Профессиональные мячи проходят обязательную сертификацию FIFA, используются для игры в
+        любую погоду&nbsp; и на любом покрытии,<span style="font-size: 10pt;"> </span>подходят для проведения тренировок
+        и соревнований профессиональных клубов.</p>
+    <p style="text-align: justify; text-indent: 18pt;">Также производители выпускают <a
+            href="/magazin/spetsialnie-myachi.html"><strong>специальные мячи</strong></a>:</p>
+    <p style="text-align: justify;">&nbsp; &nbsp; - &nbsp;для пляжного футбола <b>(</b><b><a
+                href="/magazin/spetsialnie-myachi/myach-dlya-plyazhnogo-futbola-derbystar-beach.html">beach soccer</a>)</b>
+        – специально разработаны для игры без обуви;</p>
+    <p style="text-align: justify;">&nbsp; &nbsp; - &nbsp;для уличного футбола <b>(<a
+                href="/magazin/spetsialnie-myachi/myach-dlya-ulichnogo-futbola-select-street-soccer-bl.html">street
+                soccer</a>)</b> – специально разработаны для уличных игр, <b>фристайла (</b><b>free style)</b>, имеют
+        яркий дизайн, невысокую стоимость, покрытие из твердых материалов;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    </p>
+    <p style="text-align: justify;">&nbsp; &nbsp; - облегченные, утяжеленные, &nbsp;со шнурком – специально разработаны
+        для оттачивания навыков владения мячом.</p>
+    <p style="text-align: justify; text-indent: 9pt;">&nbsp; В нашем интернет-магазине "Goal.dp.ua" представлен <strong>широкий
+            ассортимент футбольных мячей, </strong>а также для командных игр и аксессуары к ним. Наши менеджеры имеют 20
+        летний опыт в любительском и профессиональном спорте. Позвоните нам и мы поможем Вам сделать правильный выбор.
+    </p>
+    <div>Купить футбольный мяч стало проще!</div>
+</div>
